eb418bcf835a62117221323c7656ad2cacc5ad80
Dat Tran <transidattmhm@gmail.com>
2026-06-14 18:26:22 +0700

    Khoi tao Git va them bai tap tuan1, tuan2

---
+++ Tuan1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Tuan1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            /* //Biến trong C#
+            int age = 20;
+            double salary = 15000.5;
+            char grade = 'A';
+            string name = "Dat";
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            */
+            /* //Nhập dữ liệu
+            Console.Write("Nhập tên: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Xin chào: " + name);
+            */
+            /*
+            baitap b1 = new baitap();
+            b1.Bai1();
+            Console.ReadLine();
+            */
+            /*
+            baitap b2 = new baitap();
+            b2.Bai2();
+            Console.ReadLine(); 
+            */
+            baitap b3 = new baitap();
+            b3.Bai3();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tuan1/baitap.cs
@@ -0,0 +1,38 @@
+﻿namespace Tuan1
+{
+    public class baitap
+    {
+        public void Bai1()
+        {
+            Console.Write("Nhập số thứ nhất: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhập số thứ hai: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tổng = " + (a + b));
+            Console.WriteLine("Hiệu = " + (a - b));
+            Console.WriteLine("Tích = " + (a * b));
+            Console.WriteLine("Thương = " + (a / b));
+        }
+        public void Bai2()
+        {
+            Console.Write("Nhập độ C: ");
+            double c = double.Parse(Console.ReadLine());
+            double f = c * 9 / 5 + 32;
+            Console.WriteLine("Độ F = " + f);
+        }
+        public void Bai3()
+        {
+            Console.Write("Nhập cân nặng (kg): ");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhập chiều cao (m): ");
+            double height = double.Parse(Console.ReadLine());
+
+            double bmi = weight / (height * height);
+
+            Console.WriteLine("BMI = " + bmi);
+        }
+    }
+}
